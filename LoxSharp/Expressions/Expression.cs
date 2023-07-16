ef0222b16b7947e3c7b70e1433a02e8e48bac141
Mohammed Kamil <m.kamil@theahoyapp.com>
2023-07-16 17:43:03 +0400

    added record classes to capture expressions constructs

---
+++ LoxSharp/Expressions/Expression.cs
@@ -0,0 +1,11 @@
+﻿namespace LoxSharp.Expressions;
+
+public abstract record Expression;
+
+public record BinaryExpression(Expression Left, Token Operand, Expression Right) : Expression;
+
+public record Grouping(Expression Expression) : Expression;
+
+public record Literal(object Value) : Expression;
+
+public record Unary(Token Operator, Expression Right);
