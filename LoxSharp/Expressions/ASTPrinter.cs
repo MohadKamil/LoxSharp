9b06e33efe737e588b3e7a4412c825d21fd129fd
Mohammed Kamil <kamlan19922@gmail.com>
2023-07-17 13:06:30 +0400

    created print visitor

---
+++ LoxSharp/Expressions/ASTPrinter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace LoxSharp.Expressions;
+
+class ASTPrinter: IVisitor<string>
+{
+
+    public string Print(Expression expression)
+    {
+        return expression.Accept(this);
+    }
+    public string VisitBinaryExpression(BinaryExpression expression)
+    {
+        return Parenthesize(expression.Operand.Lexeme,
+            expression.Left, expression.Right);
+    }
+
+    public string VisitGroupingExpression(Grouping expression)
+    {
+        return Parenthesize("group", expression.Expression);
+    }
+
+    public string VisitLiteralExpression(Literal expression)
+    {
+        return expression.Value == null ? "nil" : expression.Value.ToString();
+    }
+
+    public string VisitUnaryExpression(Unary expression)
+    {
+        return Parenthesize(expression.Operator.Lexeme, expression.Right);
+    }
+    
+    private string Parenthesize(string name, params Expression[] expressions) {
+        var builder = new StringBuilder();
+
+        builder.Append('(').Append(name);
+        foreach (var expression in expressions)
+        {
+            builder.Append(' ');
+            builder.Append(expression.Accept(this));
+        }
+        builder.Append(')');
+        return builder.ToString();
+    }
+}
