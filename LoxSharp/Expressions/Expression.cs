2064f426fa444a767fdc2b508f7e147e270a75eb
Mohammed Kamil <kamlan19922@gmail.com>
2023-07-17 13:06:20 +0400

    updated expressions to accept a visitor

--- LoxSharp/Expressions/Expression.cs
+++ LoxSharp/Expressions/Expression.cs
@@ -1,11 +1,38 @@
 ﻿namespace LoxSharp.Expressions;
 
-public abstract record Expression;
+public abstract record Expression
+{
+    public abstract TR Accept<TR>(IVisitor<TR> visitor);
+};
 
-public record BinaryExpression(Expression Left, Token Operand, Expression Right) : Expression;
+public record BinaryExpression(Expression Left, Token Operand, Expression Right) : Expression
+{
+    public override TR Accept<TR>(IVisitor<TR> visitor)
+    {
+        return visitor.VisitBinaryExpression(this);
+    }
+}
 
-public record Grouping(Expression Expression) : Expression;
+public record Grouping(Expression Expression) : Expression
+{
+    public override TR Accept<TR>(IVisitor<TR> visitor)
+    {
+        return visitor.VisitGroupingExpression(this);
+    }
+}
 
-public record Literal(object Value) : Expression;
+public record Literal(object? Value) : Expression
+{
+    public override TR Accept<TR>(IVisitor<TR> visitor)
+    {
+        return visitor.VisitLiteralExpression(this);
+    }
+}
 
-public record Unary(Token Operator, Expression Right);
+public record Unary(Token Operator, Expression Right) : Expression
+{
+    public override TR Accept<TR>(IVisitor<TR> visitor)
+    {
+        return visitor.VisitUnaryExpression(this);
+    }
+}
