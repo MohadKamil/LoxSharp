350112e710b27270d8c3b6a187a6f0eb99c01c37
Mohammed Kamil <kamlan19922@gmail.com>
2023-07-17 13:06:14 +0400

    created a visitor to define operations on expressions

---
+++ LoxSharp/Expressions/IVisitor.cs
@@ -0,0 +1,9 @@
+﻿namespace LoxSharp.Expressions;
+
+public interface IVisitor<out TR>
+{
+    TR VisitBinaryExpression(BinaryExpression expression);
+    TR VisitGroupingExpression(Grouping expression);
+    TR VisitLiteralExpression(Literal expression);
+    TR VisitUnaryExpression(Unary expression);
+}
