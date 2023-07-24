ada0631953c1ac90362c52d01f466d29d2f10849
Mohammed Kamil <kamlan19922@gmail.com>
2023-07-24 14:42:15 +0400

    added environment class to store the variable bindings

---
+++ LoxSharp/Interpreting/LoxEnvironment.cs
@@ -0,0 +1,20 @@
+﻿namespace LoxSharp.Interpreting;
+
+public class LoxEnvironment
+{
+    private readonly IDictionary<string, object> values = new Dictionary<string, object>();
+
+    public void Define(string name, object value)
+    {
+        values[name] = value;
+    }
+
+    public object Get(Token name)
+    {
+        if (!values.ContainsKey(name.Lexeme))
+        {
+            throw new RuntimeException(name, "Undefined variable '" + name.Lexeme + "'.");
+        }
+        return values[name.Lexeme];
+    }
+}
