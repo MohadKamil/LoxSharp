9ecb1d4e938f2660d18290ef153369e2b7952505
Mohammed Kamil <m.kamil@theahoyapp.com>
2023-07-25 12:13:51 +0400

    added an if statement record

--- LoxSharp/Syntax/Statements/Statement.cs
+++ LoxSharp/Syntax/Statements/Statement.cs
@@ -7,6 +7,14 @@ public abstract record Statement
     public abstract void Accept(IStatementVisitor visitor);
 }
 
+public record IfStatement(Expression Condition, Statement ThenBranch,Statement ElseBranch) : Statement
+{
+    public override void Accept(IStatementVisitor visitor)
+    {
+        visitor.VisitIfStatement(this);
+    }
+}
+
 public record BlockStatement(IEnumerable<Statement> Statements) : Statement
 {
     public override void Accept(IStatementVisitor visitor)
