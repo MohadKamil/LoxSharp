799083288411cd0b8f8c3584055605a4dd702698
Mohammed Kamil <kamlan19922@gmail.com>
2023-07-25 18:11:02 +0400

    defined native clock function

---
+++ LoxSharp/Interpreting/NativeFunctions/Clock.cs
@@ -0,0 +1,11 @@
+﻿namespace LoxSharp.Interpreting.NativeFunctions;
+
+public class Clock : ICallable
+{
+    public object Call(Interpreter interpreter, IEnumerable<object> arguments)
+    {
+        return DateTimeOffset.Now.ToUnixTimeSeconds();
+    }
+
+    public int Arity() => 0;
+}
